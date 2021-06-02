96756a347174c0ee7d76aa8f005b2e8d8ad80f5e
arina kuznetsova <ieroarina@gmail.com>
2021-06-02 10:31:54 +0300

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+const PORT = 8888;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req,res)=>{
+    res.render('main')
+})
+app.listen(PORT, () => {
+    console.log('Server Start!!!!');
+});
