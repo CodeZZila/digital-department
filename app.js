fbce64bc763bb4bdb7b98db6cc7071804a34db05
arina kuznetsova <ieroarina@gmail.com>
2021-06-02 14:24:15 +0300

    front login main

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req,res)=>{
-    res.render('main')
+    res.render('login')
 })
 app.listen(PORT, () => {
     console.log('Server Start!!!!');
