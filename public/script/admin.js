1d3107df793ec366ae75a90de4b399f1d1a3e222
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-06-05 11:31:30 +0300

    Добавление/удаление группы

--- public/script/admin.js
+++ public/script/admin.js
@@ -28,20 +28,18 @@ $(document).ready(function () {
 });
 
 function addSubject() {
+    let abbreviation = document.getElementById('abbreviation').value
+    abbreviation=abbreviation.toUpperCase()
     $.ajax({
         url: 'admin/addSubject',           /* Куда пойдет запрос */
         method: 'post',             /* Метод передачи (post или get) */
         dataType: 'json',          /* Тип данных в ответе (xml, json, script, html). */
         data:{
             fullname: document.getElementById('fullname').value,
-            abbreviation: document.getElementById('abbreviation').value
+            abbreviation:abbreviation
         },          /* Параметры передаваемые в запросе. */
 
     });
-    console.log(document.getElementById('fullname').value);
-    console.log(document.getElementById('abbreviation').value);
-
-
 }
 
 function deleteSubject(obj) {
@@ -50,12 +48,18 @@ function deleteSubject(obj) {
         method: 'delete',             /* Метод передачи (post или get) */
         dataType: 'json',          /* Тип данных в ответе (xml, json, script, html). */
                   /* Параметры передаваемые в запросе. */
+    });
+}
+
+function addGroup() {
+    $.ajax({
+        url: 'admin/addGroup',           /* Куда пойдет запрос */
+        method: 'post',             /* Метод передачи (post или get) */
+        dataType: 'json',          /* Тип данных в ответе (xml, json, script, html). */
+        data:{
+            nameGroup: document.getElementById('fullname').value
+        },          /* Параметры передаваемые в запросе. */
 
     });
-    console.log(document.getElementById('fullname').value);
-    console.log(document.getElementById('abbreviation').value);
-
-
 }
-    
     
